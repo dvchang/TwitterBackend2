04ccf3067e0628d1b8913083c9403e97de43f16e
David Chang <dvchangyuan@gmail.com>
2023-11-16 21:14:32 -0800

    implemented all mutate operation for user

--- src/routes/userRouters.ts
+++ src/routes/userRouters.ts
@@ -1,19 +1,58 @@
 import { Router } from "express";
+import { PrismaClient } from "@prisma/client";
 
 const router = Router();
+const prisma = new PrismaClient();
 
-router.post('/', (req, res) => {
-    res.status(501).json({ error: 'Not implemnted :p' });
+router.post('/', async (req, res) => {
+    const { email, name, username } = req.body;
+    console.log("post create user", email, name, username)
+
+    try {
+        const result = await prisma.user.create({
+            data: {
+                email,
+                name,
+                username,
+                bio: "Hello, I'm new on Twitter",
+            },
+        })
+        res.status(201).json(result);
+    } catch (e) {
+        res.status(400).json({ error: "Error creating record" });
+    }
 });
 
-router.get('/:id', (req, res) => {
-    const {id} = req.params;
-    res.status(501).json({ error:`Not implemnted :p ${id}` });
+router.get('/', async (req, res) => {
+    const allUser = await prisma.user.findMany();
+    res.json(allUser)
 });
 
-router.delete('/:id', (req, res) => {
-    const {id} = req.params;
-    res.status(501).json({ error:`Not implemnted :p ${id}` });
+router.put('/:id', async (req, res) => {
+    const { id } = req.params;
+    const { bio, name, image } = req.body;
+    try {
+        const result = await prisma.user.update({
+            where: { id: Number(id) },
+            data: { bio, name, image }
+        })
+        res.json(result)
+    } catch (e) {
+        res.status(400).json({ error: "Error updating user" });
+    }
+});
+
+
+router.get('/:id', async (req, res) => {
+    const { id } = req.params;
+    const user = await prisma.user.findUnique({ where: { id: Number(id) } })
+    res.json(user)
+});
+
+router.delete('/:id', async (req, res) => {
+    const { id } = req.params;
+    await prisma.user.delete({ where: { id: Number(id) } })
+    res.send(200);
 });
 
 export default router;
