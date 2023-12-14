12d6f757cdce9027df05c81963f4856a3f35d0b3
David Chang <dvchangyuan@gmail.com>
2023-12-13 16:40:46 -0800

    Implemented follow and unfollow. tested them.

--- src/routes/userRouters.ts
+++ src/routes/userRouters.ts
@@ -1,8 +1,10 @@
-import { Router } from "express";
-import { PrismaClient } from "@prisma/client";
+import { Request, Response, Router } from "express";
+import { PrismaClient, User } from "@prisma/client";
 
 const router = Router();
 const prisma = new PrismaClient();
+
+type AuthedRequest = Request & { user?: User };
 
 router.post('/', async (req, res) => {
     const { email, name, username } = req.body;
@@ -20,6 +22,62 @@ router.post('/', async (req, res) => {
         res.status(201).json(result);
     } catch (e) {
         res.status(400).json({ error: "Error creating record" });
+    }
+});
+
+router.post('/follow', async (req: AuthedRequest, res) => {
+    const { followerId } = req.body;
+    const user: User = req.user!
+    console.log("post follow with followerId and followeeId", followerId, user.id)
+    if (!followerId) {
+        console.log("post follow missing followerId")
+        return res.status(400).json({ error: "Error follow user" });
+    }
+
+    try {
+        const result = await prisma.connection.findMany({
+            where: {
+                followerId: Number(followerId),
+                followeeId: user.id
+            }
+        })
+        if (result.length >= 1) {
+            res.status(201).json(result[0]);
+        } else {
+            try {
+                await prisma.connection.create({
+                    data: {
+                        followerId: Number(followerId),
+                        followeeId: user.id
+                    }
+                })
+                res.status(201).json(result);
+            } catch (e) {
+                console.log("post follow failed to create follow")
+                res.status(400).json({ error: "Error creating record" });
+            }
+
+        }
+    } catch (e) {
+        res.status(400).json({ error: "Error creating record" });
+    }
+});
+
+router.post('/unfollow', async (req: AuthedRequest, res) => {
+    const { followerId } = req.body;
+    const user: User = req.user!
+    console.log("post unfollow with followerId and followeeId", followerId, user.id)
+
+    try {
+        const result = await prisma.connection.deleteMany({
+            where: {
+                followerId: Number(followerId),
+                followeeId: user.id
+            }
+        })
+        res.status(201).json(result);
+    } catch (e) {
+        res.status(400).json({ error: "Error deleting record" });
     }
 });
 
